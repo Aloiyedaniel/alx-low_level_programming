88ef58ec74f9fbb0ffc42024546aa502de315315
Aloiyedaniel <aloiyedaniel>
2023-07-15 12:31:04 -0700

    vi 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-* main - Entry point of the program
-* Description: A program that prints a line using printf
-* Return: 0 (Exit success)
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-printf(" with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
