4f287d4a3e67cf465e27d383caddef4695765841
Aloiyedaniel <aloiyedaniel@gmail.com>
2023-07-15 02:17:21 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle,"; exit 0
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description: A program that prints a line using printf
+* Return: 0 (Exit success)
+*/
+int main(void)
+{
+printf("a piece of art,\n");
+return (0);
+}
