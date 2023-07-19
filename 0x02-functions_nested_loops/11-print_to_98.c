2abf5d3d1bf022d56f7c3e9a49c1dbd7731aaeb3
Aloiyedaniel <aloiyedaniel>
2023-07-19 08:47:32 -0700

    root@cf6972225568:~/alx-low_level_programming/0x02-functions_nested_loops# vi 11-print_to_98.cgit commit -m 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line
- * @n: the starting number
+ * print_to_98 - Prints all natural numbers from n to 98, followed by a new line
+ * @n: The starting number
  */
 void print_to_98(int n)
 {
