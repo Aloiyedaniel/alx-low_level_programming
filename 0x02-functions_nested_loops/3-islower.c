ca1112e5adb455d648c2d431cad987ae71d6ae88
Aloiyedaniel <aloiyedaniel>
2023-07-19 08:25:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
+ * _islower - checks if a character is lowercase
  * @c: the character to be checked
  *
- * Return: 1 if @c is a letter, 0 otherwise
+ * Return: 1 if @c is lowercase, 0 otherwise
  */
-int _isalpha(int c)
+int _islower(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
-
